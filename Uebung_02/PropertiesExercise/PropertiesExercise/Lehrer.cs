f178bfd676f0ed976b9750f327b0071346a7a43a
lucawernerspg <wer19095@spengergasse.at>
2022-10-11 11:41:43 +0200

    Commit

---
+++ Uebung_02/PropertiesExercise/PropertiesExercise/Lehrer.cs
@@ -0,0 +1,53 @@
+﻿namespace ExProperties.App
+{
+    internal class Lehrer
+    {
+
+        public string Zuname
+        {
+            get;
+            set;
+        }
+        public string Vorname
+        {
+            get;
+            set;
+        } = String.Empty;
+        private decimal? wert;
+        public decimal? Bruttogehalt
+
+        {
+            get
+            {
+                return wert;
+            }
+            set
+            {
+                if (wert == null)
+                {
+                    wert = value;
+
+                }
+
+            }
+
+        }
+
+        public decimal Nettogehalt
+        {
+            get
+            {
+                return (Bruttogehalt ?? 0) * 0.8M;
+            }
+        }
+
+        public string Kuerzel
+        {
+            get
+            {
+                return Zuname != null ? (Zuname.Length <= 3 ? Zuname.ToUpper() : Zuname[..3].ToUpper()) : string.Empty;
+            }
+        }
+
+    }
+}
